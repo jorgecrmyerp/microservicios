1bd4fe7037b2c781dce4892fe92ed00ac7f5b545
unknown <jorgecrmyerp@gmail.com>
2022-06-20 07:27:55 +0200

    lu., 20 de jun. de 2022  7:13:47 2022-06-20 PRIMERA SUBIDA DEL DIA

--- gitprune.sh
+++ gitprune.sh
@@ -15,7 +15,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date) 2022-06-17 40-RELACION ALUMNOS-CURSO-EXAMEN Y CHULETA"
+message="$(date) 2022-06-20 PRIMERA SUBIDA DEL DIA"
 fi
 
 start=`date +%s`
